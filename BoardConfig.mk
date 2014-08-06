d38535ff594c817fea08e123921c61b421ba377d
f5bud <daniel.nespoulous@gmail.com>
2014-08-06 09:59:00 +0200

    change method for boot animation
    # Device bootlogo
    TARGET_BOOTANIMATION_NAME := vertical-320x480

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,18 +4,14 @@ TARGET_ARCH_LOWMEM := true
 # Inherit from the proprietary version
 -include vendor/sony/lotus/BoardConfigVendor.mk
 
-
 # Inherit from the common montblanc definitions
 -include device/sony/montblanc-common/BoardConfigCommon.mk
-
 
 # Platform specific headers
 TARGET_SPECIFIC_HEADER_PATH += device/sony/lotus/include
 
-
 # Kernel specific configuration
 TARGET_KERNEL_CONFIG := xperiago_defconfig
-
 
 # Specific flag for LED bar notification
 COMMON_GLOBAL_CFLAGS += -DNEW_NOTIFICATION
