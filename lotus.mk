9fe2e12c26241df0d4a2894847c30115894fa667
HardLight <hardlightxda@gmail.com>
2015-01-31 13:18:25 +0200

    Remove hdpi resources
    Change-Id: I109b845df2d3c1940ad6f061db407f3bbd57553f

--- lotus.mk
+++ lotus.mk
@@ -79,7 +79,7 @@ PRODUCT_COPY_FILES += \
 # Reference: http://developer.android.com/guide/practices/screens_support.html
 # Note: In PRODUCT_AAPT_PREF_CONFIG set the proper one (e.g. hdpi), in PRODUCT_AAPT_CONFIG set the proper one and the previous one (e.g. mdpi)
 PRODUCT_AAPT_PREF_CONFIG := mdpi
-PRODUCT_AAPT_CONFIG := normal mdpi hdpi
+PRODUCT_AAPT_CONFIG := normal mdpi
 
 # Barometer permissions
 PRODUCT_COPY_FILES += \
