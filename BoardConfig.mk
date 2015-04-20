a39632cf2da32bf56dfb89e5a6f74f9d9299ed48
HardLight <hardlightxda@gmail.com>
2015-04-20 12:55:02 +0300

    CM-12.1 initial update
    Sync Agontuk
    
    Change-Id: Id61ada7345a87be213d62708435acc53c4bcbb37

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,30 +1,25 @@
 # Inherit from the proprietary version
 -include vendor/sony/lotus/BoardConfigVendor.mk
 
-
 # Inherit from the common montblanc definitions
 -include device/sony/montblanc-common/BoardConfigCommon.mk
 
-
 # Platform specific headers
 TARGET_SPECIFIC_HEADER_PATH += device/sony/lotus/include
-
 
 # Kernel specific configuration
 TARGET_KERNEL_CONFIG := xperiago_defconfig
 BOARD_CUSTOM_BOOTIMG := true
 TARGET_NO_SEPARATE_RECOVERY := true
 
-
 # Specific flag for LED bar notification
 COMMON_GLOBAL_CFLAGS += -DNEW_NOTIFICATION
-
 
 # Device identifiers for updates
 TARGET_OTA_ASSERT_DEVICE := ST27i,ST27a,lotus
 
 # TWRP
-DEVICE_RESOLUTION := 320x480
+BOARD_HAS_NO_REAL_SDCARD := true
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TARGET_RECOVERY_FSTAB := device/sony/lotus/config/fstab.st-ericsson
--- lotus.mk
+++ lotus.mk
@@ -1,70 +1,37 @@
-#
-# Copyright (C) 2014 Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
+ # Inherit from AOSP
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
-
 
 # Inherit from the common montblanc definitions
 $(call inherit-product, device/sony/montblanc-common/montblanc.mk)
 
-
 # Inherit from the device specific vendor definitions
 $(call inherit-product-if-exists, vendor/sony/lotus/lotus-vendor.mk)
-
 
 # Device specific settings overlays
 DEVICE_PACKAGE_OVERLAYS += device/sony/lotus/overlay
 
-
-# Device specific configuration scripts
+# Device specific init scripts
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/config/fstab.st-ericsson:root/fstab.st-ericsson \
 	$(LOCAL_PATH)/config/init.st-ericsson.device.rc:root/init.st-ericsson.device.rc \
-	$(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml
+	$(LOCAL_PATH)/config/init.st-ericsson.usb.rc:root/init.st-ericsson.usb.rc
 
-
-# Device specific hardware configuration script 
+# Device specific hardware configuration scripts
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/config/dash.conf:system/etc/dash.conf \
 	$(LOCAL_PATH)/prebuilt/hw_config.sh:system/etc/hw_config.sh \
 	$(LOCAL_PATH)/config/cflashlib.cfg:system/etc/cflashlib.cfg \
-	$(LOCAL_PATH)/config/flashled_param_config.cfg:system/etc/flashled_param_config.cfg
-
+	$(LOCAL_PATH)/config/flashled_param_config.cfg:system/etc/flashled_param_config.cfg \
+	$(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml
 
 # Device specific bootlogo and charging animation
-PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/prebuilt/logo-320x480.rle:root/logo.rle
-
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/logo-320x480.rle:root/logo.rle
 $(call inherit-product, device/sony/lotus/prebuilt/resources-320x480.mk)
-
-# Device specific headers
-TARGET_SPECIFIC_HEADER_PATH += device/sony/lotus/include
-
-# Bootanimation
-PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/../../../vendor/cm/prebuilt/common/bootanimation/320.zip:system/media/bootanimation.zip
 
 # TWRP
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/config/twrp.fstab:recovery/root/etc/twrp.fstab \
 	$(LOCAL_PATH)/config/init.recovery.st-ericsson.rc:root/init.recovery.st-ericsson.rc
-
-# Device specific USB configuration script
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/config/init.st-ericsson.usb.rc:root/init.st-ericsson.usb.rc
-
 
 # Device specific keylayouts and touchscreen configurations files
 PRODUCT_COPY_FILES += \
@@ -74,7 +41,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/config/cyttsp_key.kl:system/usr/keylayout/cyttsp_key.kl \
 	$(LOCAL_PATH)/config/ux500-ske-keypad.kl:system/usr/keylayout/ux500-ske-keypad.kl \
 	$(LOCAL_PATH)/config/cyttsp-spi.idc:system/usr/idc/cyttsp-spi.idc \
-
 
 # Device specific display resolutions
 # Reference: http://developer.android.com/guide/practices/screens_support.html
@@ -89,44 +55,23 @@ PRODUCT_COPY_FILES += \
 # Android kind of memory
 PRODUCT_CHARACTERISTICS := nosdcard
 
-
 # PC Companion kind of memory
 PRODUCT_PROPERTY_OVERRIDES += ro.semc.product.user_storage=emmc_only
 
-
-# Hardware video codecs configuration
+# Hardware video codecs configurations
 PRODUCT_PROPERTY_OVERRIDES += \
-  ste.video.dec.mpeg4.in.size=8192 \
-  ste.video.enc.out.buffercnt=5 \
-  ste.video.dec.recycle.delay=1 \
-  ste.video.decoder.max.hwmem=0x2600000 \
-  ste.video.decoder.max.res=720p \
-  ste.video.decoder.h264.max.lev=3.2
+	ste.video.decoder.max.hwmem=0x2600000 \
+	ste.video.decoder.max.res=720p \
+	ste.video.decoder.h264.max.lev=3.2
 
-# Reduce background apps limit to 16 on low-tier devices
+# Device density
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.sys.fw.bg_apps_limit=16
+	ro.sf.lcd_density=160
 
-# Set max background services
+# Low-RAM optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.config.max_starting_bg=8
-
-# Disable JIT code cache to free up some ram when the device is running
-PRODUCT_PROPERTY_OVERRIDES += \
-  dalvik.vm.jit.codecachesize=0
-
-# Disable atlas service on low-ram devices
-PRODUCT_PROPERTY_OVERRIDES += \
-  config.disable_atlas=true
-
-# Device specific proprieties
-# References: 
-# - http://source.android.com/devices/tuning.html
-# - http://en.wikipedia.org/wiki/Pixel_density#Calculation_of_monitor_PPI
-# - https://source.android.com/devices/low-ram.html
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.hwui.texture_cache_size=4 \
-  ro.hwui.layer_cache_size=3 \
-  ro.hwui.path_cache_size=1 \
-  ro.sf.lcd_density=160 \
-  ro.config.low_ram=true
+	ro.config.low_ram=true \
+	dalvik.vm.jit.codecachesize=0 \
+	ro.config.max_starting_bg=8 \
+	ro.sys.fw.bg_apps_limit=16 \
+	config.disable_atlas=true
